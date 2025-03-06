2dc86b2e3b5671ab16e2dcb05dc45244f62be396
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2025-03-06 20:40:51 +0100

    add possibility to load images from mod folder fixes https://github.com/SFGrenade/MoreHealing/issues/2

--- src/Consts/TextureStrings.cs
+++ src/Consts/TextureStrings.cs
@@ -9,45 +9,55 @@ public class TextureStrings
 {
     #region Misc
     public const string QuickerFocusKey = "QuickerFocus";
-    private const string QuickerFocusFile = "MoreHealing.Resources.QuickerFocus.png";
     public const string QuickestFocusKey = "QuickestFocus";
-    private const string QuickestFocusFile = "MoreHealing.Resources.QuickestFocus.png";
     public const string DeeperFocusKey = "DeeperFocus";
-    private const string DeeperFocusFile = "MoreHealing.Resources.DeeperFocus.png";
     public const string DeepestFocusKey = "DeepestFocus";
-    private const string DeepestFocusFile = "MoreHealing.Resources.DeepestFocus.png";
     #endregion Misc
 
+    private readonly string _dir;
     private readonly Dictionary<string, Sprite> _dict;
 
     public TextureStrings()
     {
+        _dir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Sprites");
+
         Assembly asm = Assembly.GetExecutingAssembly();
         _dict = new Dictionary<string, Sprite>();
-        Dictionary<string, string> tmpTextures = new Dictionary<string, string>();
-        tmpTextures.Add(QuickerFocusKey, QuickerFocusFile);
-        tmpTextures.Add(QuickestFocusKey, QuickestFocusFile);
-        tmpTextures.Add(DeeperFocusKey, DeeperFocusFile);
-        tmpTextures.Add(DeepestFocusKey, DeepestFocusFile);
+        List<string> tmpTextures = new List<string>();
+        tmpTextures.Add(QuickerFocusKey);
+        tmpTextures.Add(QuickestFocusKey);
+        tmpTextures.Add(DeeperFocusKey);
+        tmpTextures.Add(DeepestFocusKey);
         foreach (var t in tmpTextures)
         {
-            using (Stream s = asm.GetManifestResourceStream(t.Value))
+            //Create texture from bytes
+            var tex = new Texture2D(2, 2);
+
+            string filePath = Path.Combine(_dir, t);
+            if (Directory.Exists(_dir) && File.Exists(filePath))
             {
-                if (s == null) continue;
-
-                byte[] buffer = new byte[s.Length];
-                s.Read(buffer, 0, buffer.Length);
-                s.Dispose();
-
-                //Create texture from bytes
-                var tex = new Texture2D(2, 2);
-
-                tex.LoadImage(buffer, true);
-
-                // Create sprite from texture
-                // Split is to cut off the TestOfTeamwork.Resources. and the .png
-                _dict.Add(t.Key, Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f)));
+                // when file exists, use that
+                tex.LoadImage(File.ReadAllBytes(filePath));
             }
+            else
+            {
+                // otherwise, embedded resource
+                using (Stream s = asm.GetManifestResourceStream($"MoreHealing.Resources.{t}.png"))
+                {
+                    if (s == null) continue;
+
+                    byte[] buffer = new byte[s.Length];
+                    s.Read(buffer, 0, buffer.Length);
+                    s.Dispose();
+
+
+                    tex.LoadImage(buffer, true);
+                }
+            }
+
+            // Create sprite from texture
+            // Split is to cut off the TestOfTeamwork.Resources. and the .png
+            _dict.Add(t, Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f)));
         }
     }
 
